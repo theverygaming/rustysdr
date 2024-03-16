f7923fa843d95ed756891eacee12b4c00bcba3c8
theverygaming <theverygaming@users.noreply.github.com>
2024-03-16 23:53:39 +0100

    upload built windows binaries

--- create_windows_archive.ps1
+++ create_windows_archive.ps1
@@ -1,14 +1,14 @@
 $build_dir=$args[0]
 
-mkdir rustysdr_windows_x64
+mkdir rustysdr_windows_pkg
 
 # main executable
 cp $build_dir/release/silly2.exe rustysdr_windows_pkg/
 
 # dynamic libraries
-cp $build_dir/release/release/build/libvok-sys-*/out/lib/libvolk.dll rustysdr_windows_pkg/
-cp $build_dir/release/build/fftw-src/*/out/libfftw3-3.dll rustysdr_windows_pkg/
-cp $build_dir/release/build/fftw-src/*/out/libfftw3f-3.dll rustysdr_windows_pkg/
+cp $build_dir/release/build/libvolk-sys-*/out/lib/libvolk.dll rustysdr_windows_pkg/
+cp $build_dir/release/build/fftw-src-*/out/libfftw3-3.dll rustysdr_windows_pkg/
+cp $build_dir/release/build/fftw-src-*/out/libfftw3f-3.dll rustysdr_windows_pkg/
 
 
 Compress-Archive -Path rustysdr_windows_pkg/ -DestinationPath rustysdr_windows.zip
