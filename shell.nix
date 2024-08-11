0d606211812e0dd6d78e1d974b700c89291787e6
theverygaming <theverygaming@users.noreply.github.com>
2024-08-11 09:52:10 +0200

    upgrade to volk 3.1.2

--- shell.nix
+++ shell.nix
@@ -1,4 +1,4 @@
-with import <nixpkgs> { };
+with import (fetchTarball https://github.com/NixOS/nixpkgs/archive/5e0ca22929f3342b19569b21b2f3462f053e497b.tar.gz) { };
 stdenv.mkDerivation {
   name = "rustysdr";
   buildInputs = [
