bde9075b7818dd06530eef396a90ae48ae2af7f3
theverygaming <theverygaming@users.noreply.github.com>
2024-08-11 21:28:48 +0200

    renamed stream variables

--- dsp/src/stream.rs
+++ dsp/src/stream.rs
@@ -8,9 +8,9 @@ pub struct Stream<T> {
     pub buf_read: Mutex<AlignedVec<T>>,
     write_size: Mutex<usize>,
     read_done: Mutex<bool>,
-    read_done_cv: Condvar,
-    write_done: Mutex<bool>,
-    write_done_cv: Condvar,
+    write_cv: Condvar,
+    data_ready: Mutex<bool>,
+    read_cv: Condvar,
 }
 
 impl<T> Stream<T> {
@@ -20,9 +20,9 @@ impl<T> Stream<T> {
             buf_read: Mutex::new(AlignedVec::new_zeroed(stream_size)),
             write_size: Mutex::new(0),
             read_done: Mutex::new(true),
-            read_done_cv: Condvar::new(),
-            write_done: Mutex::new(false),
-            write_done_cv: Condvar::new(),
+            write_cv: Condvar::new(),
+            data_ready: Mutex::new(false),
+            read_cv: Condvar::new(),
         })
     }
 
@@ -32,15 +32,15 @@ impl<T> Stream<T> {
         // eep until a read is done
         let mut read_done = self.read_done.lock().unwrap();
         while !*read_done {
-            read_done = self.read_done_cv.wait(read_done).unwrap();
+            read_done = self.write_cv.wait(read_done).unwrap();
         }
 
         std::mem::swap(&mut *self.buf_write.try_lock().unwrap(), &mut *self.buf_read.try_lock().unwrap());
         *read_done = false;
 
         *self.write_size.lock().unwrap() = n;
-        *self.write_done.lock().unwrap() = true;
-        self.write_done_cv.notify_all();
+        *self.data_ready.lock().unwrap() = true;
+        self.read_cv.notify_all();
 
         true
     }
@@ -48,9 +48,9 @@ impl<T> Stream<T> {
     // reads data from the buffer, returns size written, Option is None when the reader is stopped
     pub fn read<'a>(self: &'a Arc<Self>) -> Option<usize> {
         // eep until a write is done
-        let mut write_done = self.write_done.lock().unwrap();
-        while !*write_done {
-            write_done = self.write_done_cv.wait(write_done).unwrap();
+        let mut data_ready = self.data_ready.lock().unwrap();
+        while !*data_ready {
+            data_ready = self.read_cv.wait(data_ready).unwrap();
         }
 
         Some(*self.write_size.lock().unwrap())
@@ -58,8 +58,8 @@ impl<T> Stream<T> {
 
     // called when reading has been finished
     pub fn flush(self: &Arc<Self>) {
-        *self.write_done.lock().unwrap() = false;
+        *self.data_ready.lock().unwrap() = false;
         *self.read_done.lock().unwrap() = true;
-        self.read_done_cv.notify_all();
+        self.write_cv.notify_all();
     }
 }
