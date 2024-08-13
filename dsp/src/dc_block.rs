baed1ddc60d9dc909701fe1caa087bbe375bb90b
theverygaming <theverygaming@users.noreply.github.com>
2024-08-13 17:34:06 +0200

    implement dc_block functionality

--- dsp/src/dc_block.rs
+++ dsp/src/dc_block.rs
@@ -15,14 +15,14 @@ pub struct DcBlock<T> {
 }
 
 impl DcBlock<f32> {
-    pub fn new(stream_size: usize) -> Arc<Self> {
-        Arc::new(DcBlock {
+    pub fn new(stream_size: usize) -> Arc<Mutex<Self>> {
+        Arc::new(Mutex::new(DcBlock {
             offset: 0.0,
             rate: 0.0001,
             input: Stream::new(stream_size),
             output: Stream::new(stream_size),
             thread_handle: None,
-        })
+        }))
     }
 }
 
@@ -42,47 +42,34 @@ crate::impl_block!(DcBlock, DcBlockImpl);
 
 impl DcBlockImpl for DcBlock<f32> {
     fn run(&mut self) -> bool {
+        let n_read = self.input.read().unwrap();
+        let buf_r = self.input.buf_read.lock().unwrap();
+        let mut buf_w = self.output.buf_write.lock().unwrap();
+
+        for i in 0..n_read {
+            buf_w[i] = buf_r[i] - self.offset;
+            self.offset += buf_w[i] * self.rate;
+        }
+
+        self.output.swap(n_read);
+        self.input.flush();
         true
     }
 }
 
 impl DcBlockImpl for DcBlock<Complex<f32>> {
     fn run(&mut self) -> bool {
+        let n_read = self.input.read().unwrap();
+        let buf_r = self.input.buf_read.lock().unwrap();
+        let mut buf_w = self.output.buf_write.lock().unwrap();
+
+        for i in 0..n_read {
+            buf_w[i] = buf_r[i] - self.offset;
+            self.offset += buf_w[i] * Complex {re: self.rate, im: self.rate};
+        }
+
+        self.output.swap(n_read);
+        self.input.flush();
         true
     }
 }
-
-
-/*
-
-impl DspBlock<f32> for DcBlock<f32> {
-    fn process(&mut self, input: &mut [f32], output: &mut [f32]) {
-        for i in 0..input.len() {
-            output[i] = input[i] - self.offset;
-            self.offset += output[i] * self.rate;
-        }
-    }
-
-    fn compute_output_size(&mut self, input_size: usize) -> usize {
-        input_size
-    }
-
-    fn set_input_size(&mut self, input_size: usize) {}
-}
-
-impl DspBlock<Complex<f32>> for DcBlock<Complex<f32>> {
-    fn process(&mut self, input: &mut [Complex<f32>], output: &mut [Complex<f32>]) {
-        for i in 0..input.len() {
-            output[i] = input[i] - self.offset;
-            self.offset += output[i] * Complex {re: self.rate, im: self.rate};
-        }
-    }
-
-    fn compute_output_size(&mut self, input_size: usize) -> usize {
-        input_size
-    }
-
-    fn set_input_size(&mut self, _input_size: usize) {}
-}
-
-*/
