6e58ec2c0c965d40ce22bee9f36ba4317b9561a5
theverygaming <theverygaming@users.noreply.github.com>
2025-10-26 10:21:10 +0100

    silly: update tungstenite and improve error handling

--- silly/src/main.rs
+++ silly/src/main.rs
@@ -4,7 +4,7 @@ use std::fs::File;
 
 fn main() {
     let refcount_reader = std::sync::Arc::new(std::sync::Mutex::new(
-        dsp::libwav::Reader::new(File::open("/home/user/Downloads/rustysdr/doom.wav").unwrap(), true).unwrap(),
+        dsp::libwav::Reader::new(File::open("/home/user/Downloads/thebestfuckingmicrophone.wav").unwrap(), true).unwrap(),
     ));
     //let reader = dsp::libwav::Reader::new(File::open(std::env::temp_dir().join("/home/user/Downloads/rustysdr/doom.wav")).unwrap(), true).unwrap();
     //let mut writer = dsp::libwav::Writer::new(File::create(std::env::temp_dir().join("/home/user/Downloads/rustysdr/out.wav")).unwrap(), reader.get_samplerate(), reader.get_channels(), reader.get_sample_format()).unwrap();
@@ -20,22 +20,28 @@ fn main() {
         std::thread::spawn(move || {
             let mut counter: u64 = 0;
             let mut websocket = tungstenite::accept(stream.unwrap()).unwrap();
+            let mut buffer: AlignedVec<f32> = AlignedVec::from_elem(0.0, 4800);
             loop {
-                let msg = websocket.read().unwrap(); // TODO: handle ConnectionClosed
-
+                let msg = match websocket.read() {
+                    Ok(v) => v,
+                    Err(tungstenite::error::Error::ConnectionClosed) => {
+                        println!("closing WS");
+                        return;
+                    }
+                    Err(e) => panic!("WS error: {:?}", e),
+                };
                 // We do not want to send back ping/pong messages.
                 //if msg.is_binary() || msg.is_text() {
                 //    websocket.send(msg).unwrap();
                 //}
-                let mut buffer: AlignedVec<f32> = AlignedVec::from_elem(0.0, 4800);
                 reader_clone.lock().unwrap().read_samples(&mut buffer).unwrap();
-                let e: Vec<u8>;
-                unsafe {
-                    e = std::slice::from_raw_parts(buffer.as_ptr() as *const u8, buffer.len() * std::mem::size_of::<f32>()).to_vec();
-                }
-                websocket.send(tungstenite::Message::Text(counter.to_string()));
+                let audiobytes = unsafe {
+                    let slice = std::slice::from_raw_parts(buffer.as_ptr() as *const u8, buffer.len() * std::mem::size_of::<f32>());
+                    tungstenite::Bytes::copy_from_slice(slice)
+                };
+                websocket.send(tungstenite::Message::Text(counter.to_string().into()));
                 counter += 1;
-                websocket.send(tungstenite::Message::Binary(e));
+                websocket.send(tungstenite::Message::Binary(audiobytes));
             }
         });
     }
