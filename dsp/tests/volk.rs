4ea17ab5da9a8f635a45a4723925a430d423e43a
theverygaming <18639279+theverygaming@users.noreply.github.com>
2026-05-07 08:46:31 +0200

    compat with new volk-rs

--- dsp/tests/volk.rs
+++ dsp/tests/volk.rs
@@ -5,6 +5,6 @@ fn volk_works() {
     let input: AlignedVec<core::ffi::c_short> = AlignedVec::from_elem(1, 500);
     let mut taps: AlignedVec<Complex<f32>> = AlignedVec::from_elem(Complex { re: 5.0, im: 2.0 }, 500);
     let mut result: Complex<f32> = Complex { re: 0.0, im: 0.0 };
-    volk_rs::kernels::volk_16i_32fc_dot_prod_32fc(&input, &mut result, &mut taps);
+    volk_rs::kernels::volk_16i_32fc_dot_prod_32fc(&mut result, &input, &mut taps);
     assert!(result.re != 0.0 && result.im != 0.0, "borked");
 }
